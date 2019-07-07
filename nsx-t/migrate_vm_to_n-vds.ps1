66b86555365887b5ee3895bb15822dd94dc58d56
Søren Isager Høvring <51338751+sorenisager@users.noreply.github.com>
2019-07-07 23:37:13 +0200

    Update migrate_vm_to_n-vds.ps1

--- nsx-t/migrate_vm_to_n-vds.ps1
+++ nsx-t/migrate_vm_to_n-vds.ps1
@@ -1,2 +1,16 @@
 # Script to automate the migration from DvS / Or any Port-group / To N-VDS
 # Created by Sorenisager, sorenisager.com - free to change.
+
+# Variables
+$SwitchDestinationName = "NSX_GLOBAL_IX_1110" # Destination NSX-T switch
+$SourceSwitchName = "DvS_IX_GLOBAL" # Source switch
+$ClusterToMigrate = "PROD_IX_WIN01" # Name of your cluster
+
+# Get VMs
+$vms = Get-VirtualPortGroup -name $SourceSwitchName | Get-VM  | Where-Object {$_.VMHost.Parent -like "$($ClusterToMigrate)"}
+
+foreach ($vm in $vms)
+  {
+    # Set the new N-VDS Segment name
+      $vm | Get-NetworkAdapter -NetworkName $SourceSwitchName | Set-NetworkAdapter -NetworkName $SwitchDestinationName -Confirm:$false
+  } 
